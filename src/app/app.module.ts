dbb5c9285a6bb1d8ec8af07aceab92a09b3b4f38
9erch <ghanmi.khalil@gmail.com>
2023-03-30 06:39:05 +0100

    6549641

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestCompoComponent } from './test-compo/test-compo.component';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCompoComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/first/first.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // Mettre ici le code à exécuter lors de l'initialisation du composant
+  }
+
+}
